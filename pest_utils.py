f34c5b2ff81f1bacd0ee3dcef0125c21d7eca0f2
Ryma AISSAT <46969610+aissatryma@users.noreply.github.com>
2019-02-20 11:23:00 +0100

    Update pest_utils.py

--- pest_utils.py
+++ pest_utils.py
@@ -35,30 +35,36 @@ def write_obs_data(obs_points, input_dir, output_dir, dates_out, date_string_for
 
     date_string_format : format of date strings in observation files (e.g.  '%Y-%m-%d')
 
+    dates_out : Sequence of fixed frequency DatetimeIndex 
+
     Return
     ------
     Number of observations (obs_num)
     Number of observation groups (nobs_grp)
     Dictionary with  observation dates :  obs_dates = {'ID1':[date1,...daten], ... }
     '''
+
     # initialize dictionary of observation dates
+
     obs_dates = {}
     # open pst files
     try :
-        pst_observation_groups_file= open(output_dir + 'pst_observation_groups.txt', 'w')
-        pst_io_file = open(output_dir +   'pst_io_obs.txt', 'w')
+        pst_observation_groups_file = open(output_dir + 'pst_observation_groups.txt', 'w')
+        pst_io_file   = open(output_dir +   'pst_io_obs.txt', 'w')
         obs_data_file = open(output_dir + 'pst_obs_list.txt', 'w')
+
 
     except :
         print(('Write error in ' + output_dir ))
         return
 
     # init observations and observation groups counters
-    obs_num = 0
+    obs_num  = 0
     nobs_grp = 0
 
     # width of values (number of characters)
-    VAL_CHAR_LEN = 12
+    VAL_CHAR_LEN = 21
+    VAL_START = 12
 
     # Start function for each observation file
     for obs_point in obs_points:
@@ -67,18 +73,17 @@ def write_obs_data(obs_points, input_dir, output_dir, dates_out, date_string_for
         obs_point_file_path = input_dir + obs_point + '.txt'
 
         try :
-            obs_point_array = np.genfromtxt(obs_point_file_path,
-                delimiter= '\t',skip_header=True,unpack=True,
-                converters={ 0: mdates.bytespdate2num('%Y')}
-                )
+            df_obs_point = pd.read_csv(obs_point_file_path,sep='\t')
+            df_obs_point.Year = pd.to_datetime(df_obs_point.Year,  format="%Y-%m-%d")
+            df_obs_point = df_obs_point.set_index(df_obs_point.Year)
             print(('Successfully read ' + obs_point + ' observation file.'))
         except :
             print(('Cannot find ' + obs_point + ' observation file.'))
             continue
 
-        obs_point_datenums = obs_point_array[:,0]
-        obs_point_values = obs_point_array[:,1]
-        obs_point_weight = obs_point_array[:,2]
+        obs_point_datenums = df_obs_point.Year
+        obs_point_values   = df_obs_point.Mean
+        obs_point_weight   = df_obs_point.Weight
 
         # Interpolate / subset observed values
         # Interpolate missing values
@@ -86,9 +91,11 @@ def write_obs_data(obs_points, input_dir, output_dir, dates_out, date_string_for
             select_obs_values = interp_from_file(obs_point_file, dates_out, date_string_format = date_string_format)
         # Look for available observed values at simulation dates (dates_out)
         else :
-            idx = np.in1d(obs_point_datenums, obs_point_datenums)# mdates.date2num(dates_out))
-            select_obs_values = obs_point_values[idx]
-            select_obs_weight = obs_point_weight[idx]
+            
+            select_obs_values = obs_point_values[dates_out]
+            select_obs_weight = obs_point_weight[dates_out]
+
+           
 
         # fill obs_dates dictionary
         obs_dates[obs_point]= obs_point_datenums
@@ -102,12 +109,12 @@ def write_obs_data(obs_points, input_dir, output_dir, dates_out, date_string_for
         # Write instruction and observatin line for each value
         for obs_val, obs_weight in zip(select_obs_values, select_obs_weight):
             obs_num += 1
-            ins_file.write('l1 ' + '(o' + str(obs_num) + ')' + '1:' + str(VAL_CHAR_LEN) + '\n')
+            ins_file.write('l1 ' + '(o' + str(obs_num) + ')' + str(VAL_START) +':' + str(VAL_CHAR_LEN) + '\n')
             obs_data_file.write('o' + str(obs_num) + ' ' + str(obs_val) + ' ' + str(obs_weight) + ' ' + str(obs_point) + '\n')
 
         # add point entry to pst files
         pst_observation_groups_file.write( obs_point + '\n')
-        pst_io_file.write( 'pest_files/' + obs_point + '.ins' + ' ' + 'pest_files/' + obs_point + '.dat' + '\n' )
+        pst_io_file.write( 'pest_files/' + obs_point + '.ins' + ' ' + 'pest_files/' + obs_point + '.txt' + '\n' )
 
         # close point files
         ins_file.close()
@@ -120,29 +127,30 @@ def write_obs_data(obs_points, input_dir, output_dir, dates_out, date_string_for
     pst_io_file.close()
     obs_data_file.close()
 
-    return( obs_num, nobs_grp, obs_dates )
+    return( obs_num, nobs_grp, obs_dates)
 
 
+
+'''
 def write_tpl_files(dic_params, output_dir, dic_params_init = None):
     '''
-    Description
-    -----------
-    Computation of template files for pest
+    #Description
+    #-----------
+    #Computation of template files for pest
 
-    Parameters
-    ----------
-    dic_params : dict
-            Dict with groups of calibration in keys with parameters names inside
-        ex: dic_params = {'Hydrodynamic':['T','S'], 'Recharge':['CRT','DCRT','FN','QImax','QRmax','CQI','CQR']}
-    output_dir : str
-            path to the output directory where template files will be written
-    dic_params_init : dict
-            initial parameter values
-            ex: dic_params_init = {'Hydrodynamic':[0.01,0.1], 'Recharge':[80, 5, 10, 130, 80, 0.05, 0.3]}
-    Returns
-    -------
-
-    '''
+    #Parameters
+    #----------
+    #dic_params : dict
+     #       Dict with groups of calibration in keys with parameters names inside
+      #  ex: dic_params = {'Hydrodynamic':['T','S'], 'Recharge':['CRT','DCRT','FN','QImax','QRmax','CQI','CQR']}
+    #output_dir : str
+     #       path to the output directory where template files will be written
+    #dic_params_init : dict
+    #        initial parameter values
+     #       ex: dic_params_init = {'Hydrodynamic':[0.01,0.1], 'Recharge':[80, 5, 10, 130, 80, 0.05, 0.3]}
+    #Returns
+   #-------
+'''
     # iterate over parameter groups
     pst_par_grp_file_path = output_dir + 'pst_par_grp.txt'
     pst_par_data_file_path = output_dir + 'pst_par_data.txt'
@@ -203,39 +211,4 @@ def write_pst_io(path_output_files) :
     except :
         print('Cannot find pst_io_par.txt or pst_io_obs.txt')
 
-
-# ---------------------------------------------------------------
-def gen_time_seq(date_start_string, tstep, n_tstep, date_string_format = '%Y-%m-%d %H:%M' ) :
-    """
-    Description.
-
-    Parameters
-    ----------
-    date_start_string : date from which to start the sequence in string format
-                        '%Y-%m-%d %H:%M', e.g. '2015-03-02 12:00'.
-    tstep : length of time step in seconds.
-    n_tstep : number of time steps
-    date_string_format : date string format used in input file,
-                         see https://docs.python.org/2/library/datetime.html#strftime-strptime-behavior
-
-    Returns
-    -------
-
-    dates_out : list of datetime objects
-
-    Examples
-    --------
-    # generate a sequence of 20 datetimes instances starting from 2015-03-02 12:00 at 1-minute-interval.
-    >>> dates_out = gen_time_seq(date_start_string = '2015-03-02 12:00' ,tstep = 60 ,n_tstep = 20)
-
-    """
-    # convert date_start_string to datetime format
-    date_start = dt.datetime.strptime(date_start_string, date_string_format)
-    # estimate date_end in datetime format
-    date_end = date_start + dt.timedelta(seconds = tstep*n_tstep)
-    # generate sequence of dates
-    datenums_out = mdates.drange(date_start,date_end - dt.timedelta(seconds=0.001), dt.timedelta(seconds=tstep))
-    # convert to list of datetime
-    dates_out = mdates.num2date(datenums_out)
-
-    return(dates_out)
+'''
