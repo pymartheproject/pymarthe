4ce1248394c57b7ded1fb5e13bf58a399ba682c2
PRYET Alexandre <apryet@hydrolix.zut.ipb.fr>
2019-07-25 17:12:28 +0200

    after resolving repos update issues, attempt to fix NA handling

--- pymarthe/mobs.py
+++ pymarthe/mobs.py
@@ -69,9 +69,10 @@ class MartheObs() :
         df.set_index('date', inplace = True)
 
         # convert no data values to nan 
-        for no_data_string in NO_DATA_VALUES :
-            df.loc[df.value == no_data_string,'value'] = np.nan
-			df.dropna()
+        # NOTE : issues with incomplete series simul/obs mismatch
+	#for no_data_string in NO_DATA_VALUES :
+        #    df.loc[df.value == no_data_string,'value'] = np.nan
+	#		df.dropna()
 
         self.df = df
         
