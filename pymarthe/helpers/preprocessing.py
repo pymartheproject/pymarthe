3fd160ba8b5daee57a08ea237fd70cd2f80f595f
pmatran <pierre.matran@outlook.fr>
2022-07-19 23:04:58 +0200

    Add helpers.preprocessing.extract_model_geology() fonction to request BDCharm50 BRGM database and extract geological information on a Marthe model extension as shapefile.

--- pymarthe/helpers/preprocessing.py
+++ pymarthe/helpers/preprocessing.py
@@ -8,6 +8,7 @@ import re
 import numpy as np
 import pandas as pd
 from copy import deepcopy
+from pymarthe import MartheModel
 from pymarthe.utils import *
 
 
@@ -107,3 +108,88 @@ def spatial_aggregation(mm, x, y, layer, value, agg = 'sum', trans ='none', only
 
 
 
+
+
+def extract_model_geology(mm, shpout, epsg=2154, on_grid=False):
+    """
+    Extract geology on a projected Marthe Model extension from BRGM 
+    BDCharm50 data base.
+    (only available for projected model located in France country).
+
+    Parameters
+    ----------
+    mm (MartheModel) : MARTHE model to consider.
+
+    shpout (str) : output shapefile name. 
+
+    epsg (int, optional) : Geodetic Parameter Dataset.
+                           Default is 2154 (RGF93).
+
+    on_grid (bool, optional) : whatever set geological data on grid cell.
+                               If False, real geological polygons on model 
+                               extension will be written.
+                               If True, a spatial join operation will be 
+                               performed between grid and geological data.
+                               Default is False.
+
+
+    Examples
+    --------
+    mm = MartheModel('mymodel.rma')
+    write_model_geology(mm, 'Lambert_3_model_geol.shp', epsg=27572, on_grid=True)
+
+    """
+    # ---- Import all additional required python modules
+    try:
+        import requests, shutil
+        from shapely.geometry import Polygon
+        import geopandas as gpd
+    except:
+        pcks = ['requests','shutil','shapely','geopandas']
+        err_msg = 'Could not import all the required python module(s):\n\t- '
+        err_msg += '\n\t- '.join(pcks)
+        ImportError(err_msg)
+
+    # ---- Query BDCharm50 data base online
+    print('Query BRGM `BDCharm50` data base online ...')
+    r = requests.get("http://infoterre.brgm.fr/telechargements/BDCharm50/FR_vecteur.zip")
+
+    # ---- Create _temp_ working folder and download data
+    if os.path.exists('_temp_'):
+        shutil.rmtree('_temp_')
+    os.mkdir('_temp_')
+    open(os.path.join('_temp_', '_temp_.zip'), 'wb').write(r.content)
+
+    # ---- Read geology shapefile of whole France country 
+    #     (since `bbox` and mask attribut doesn't work on 0.11.0)
+    print('Read geological data ...')
+    data = "zip://_temp_/_temp_.zip!FR_vecteur/GEO001M_CART_FR_S_FGEOL_2154.shp"
+    gdf = gpd.read_file(data).to_crs(epsg)
+
+    # ---- Manage whatever set geological data on grid
+    if on_grid:
+        # -- Build `.modelgrid` if not exists
+        if mm.modelgrid is None:
+            mm.build_modelgrid()
+        # -- Convert grid to Polygons GeoDataFrame
+        print('Processing geological data on model grid ...')
+        geoms = mm.modelgrid['vertices'].apply(Polygon)
+        grid = gpd.GeoDataFrame( mm.modelgrid, geometry=geoms, crs=epsg
+                               ).drop_duplicates('geometry')
+        # -- Perform spatial join between geology and modelgrid
+        out = grid.sjoin(gdf).drop(columns='index_right')
+    else:
+        # ---- Extract model extension as bounding box geometry
+        print('Processing geological data on model extension ...')
+        bbox = gpd.GeoDataFrame(geometry=[gpd.base.box(*mm.get_extent())], crs=epsg)
+        out = gdf.overlay(bbox, how='intersection')
+
+    # ---- Convert columns names to lower case
+    out.columns = out.columns.str.lower()
+
+    # ---- Export to shapefile
+    out.drop(columns='vertices').to_file(shpout, index=False)
+    print(f'Shapefile {shpout} have been written successfully!')
+
+    # ---- Remove temporary folder
+    shutil.rmtree('_temp_')
